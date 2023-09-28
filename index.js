8a12e744a4328fbd5595bcc440b7ad3f6f05fbc1
Lubnabd <lubna.abdelkhaleq@gmail.com>
2023-09-28 10:55:07 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "John Doe",
+    streetAddress: "123 Main Street"
+  };
+
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = { ...employee };
+  
+    updatedEmployee[key] = value;
+  
+    return updatedEmployee;
+  }
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+console.log();
+    employee[key] = value;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    
+    const newEmployee = { ...employee };
+  
+    delete newEmployee[key];
+  
+    return newEmployee;
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+  
+  }
